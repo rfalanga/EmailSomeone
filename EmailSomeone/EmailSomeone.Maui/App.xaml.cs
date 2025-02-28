4c9b601fcf392950c580ee7eaf9f6a9edaa947af
Rod Falanga <rfalanga@users.noreply.github.com>
2025-02-28 07:42:28 -0700

    Initial commit

---
+++ EmailSomeone/EmailSomeone.Maui/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EmailSomeone.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "EmailSomeone.Maui" };
+        }
+    }
+}
---
+++ EmailSomeone/EmailSomeone.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailSomeone.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
